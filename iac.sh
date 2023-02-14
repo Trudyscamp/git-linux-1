119f6c972bd3ee256b7632ff20a9b5e945733f3e
Trudyscamp <eduardo.vynnicius@gmail.com>
2023-02-14 23:55:26 +0000

    ARQUIVO IAC V.1

---
+++ iac.sh
@@ -0,0 +1,43 @@
+
+#!/bin/bash
+
+echo "Criando os Diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando os grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando Usuários..."
+
+useradd Carlos -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_ADM
+useradd Maria -m -s /bin/bash -p $(openssl passwd -6 123)  -G GRP_ADM
+useradd Joao -m -s /bin/bash -p $(openssl passwd -6 123)   -G GRP_ADM
+
+useradd Debora -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_VEN
+useradd Sebastiana -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_VEN
+useradd Roberto -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_VEN
+
+useradd Josefina -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_SEC
+useradd Amanda -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_SEC
+useradd Rogerio -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_SEC
+
+echo "Criando permissões aos diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Fim do script....."
+
